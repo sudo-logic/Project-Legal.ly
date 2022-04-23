c33a056eff94271c937b43624d5f0d5e328077dd
Logeek <awesomelogix@gmail.com>
2022-04-23 13:17:48 +0530

    Added fuzzy logic to backend

--- legal_ly/backend/app.py
+++ legal_ly/backend/app.py
@@ -1,22 +1,18 @@
-# Flask app to accept chat messages and respond with a response
 
+# Makeshift script for chatbot backend as the Model couldn't train in time :')
+# Please read the README file to check our actual approach towards the problem
 
 from email import header
 from flask import Flask, request, render_template
-# from chatterbot import ChatBot
-# from chatterbot.trainers import ChatterBotCorpusTrainer
 from flask_cors import CORS, cross_origin
 from matplotlib.pyplot import text
 import pandas as pd
+import difflib
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-
-# english_bot = ChatBot("Ron Obvious")
-# trainer = ChatterBotCorpusTrainer(english_bot)
-# trainer.train("chatterbot.corpus.english")
 
 data = pd.read_csv('A2015-22.csv')
 
@@ -24,19 +20,31 @@ topics = list(data['title'])
 text = list(data['text'])
 
 
-# print(topics)
+greetings = ['Hi', 'Hello', "Hey There", 'hi']
 
 @app.route("/api", methods=['POST'])
 @cross_origin()
 def get_bot_response():
     msg = request.form['msg']
-    print(msg)
-    for i in topics:    
-        print(i)
-        if msg.lower().strip() == i.lower().strip():
-            return text[topics.index(msg)]
+
+    match = difflib.get_close_matches(msg, topics, n=1)
+
+    print(difflib.get_close_matches(msg, greetings))
+    
+    if difflib.get_close_matches(msg, greetings):
+        return "Hi There! Welcome to Legal.ly\nPlease type 'topics' to get a list of the topics I have knowledge on."
+    
+    print(msg.lower().strip())
+    
+    if msg.lower().strip() in ['topics', 'topic']:
+        response = 'You can ask me anything about the following topics:\n' + " | ".join(topics)
+        return response
+
+    if match:
+        match = match[0]
+        return text[topics.index(match)]
     else:
-        return "Sorry, I don't know that topic"
+        return "Sorry, I didn't understand that."
 
 if __name__ == "__main__":
     app.run("0.0.0.0", debug=True)
